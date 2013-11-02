56c5c3b74ab5f20e68506e3cebdd3920dbb78de8
Alan Foster <alan@alanfoster.me>
2013-11-02 19:59:04 +0000

    Split and comment EditorTest now that it works as expected. Unsure how to best test the UI.

---
+++ src/foo/editor/EipEditor.scala
@@ -0,0 +1,129 @@
+package foo.editor
+
+import com.intellij.openapi.fileEditor.{FileEditorStateLevel, FileEditorState, FileEditorLocation, FileEditor}
+import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.project.Project
+import com.intellij.ide.structureView.StructureViewBuilder
+import com.intellij.codeHighlighting.BackgroundEditorHighlighter
+import java.beans.PropertyChangeListener
+import javax.swing.{JLabel, JPanel, JComponent}
+
+/**
+ * Creates and visualises the given XML DSl as a graph.
+ */
+class EipEditor(project: Project, file: VirtualFile) extends UserDataHolderBase with FileEditor {
+  /**
+   * Represents the constant tab name, for improved user experience
+   */
+  val tabName = "Eip Editor"
+
+  // todo
+  val jpanel = new JPanel()
+  jpanel.add(new JLabel("hiya"))
+
+  /**
+   * Disposes resources associated with this editor
+   */
+  def dispose() {
+    // noop - swing will automatically dispose of itself when de-referenced
+  }
+
+  /**
+   * Removes the specified listener
+   * @param listener The listener
+   */
+  def removePropertyChangeListener(listener: PropertyChangeListener) {
+    // noop
+  }
+
+  /**
+   * Adds the specified listener
+   * @param listener The listener
+   */
+  def addPropertyChangeListener(listener: PropertyChangeListener) {
+    // noop
+  }
+
+  /**
+   * @inheritdoc
+   */
+  def deselectNotify() {
+    // noop
+  }
+
+  /**
+   * @inheritdoc
+   */
+  def selectNotify() {
+    // noop
+  }
+
+  /**
+   *  The EIP editor is stateless. As such, this is noop.
+   * @param state
+   */
+  def setState(state: FileEditorState) {
+
+  }
+
+  /**
+   * Represents the component to show within the editor region
+   * @return the EIP graph component
+   */
+  def getComponent: JComponent = jpanel
+
+  /**
+   * Places focus on the component when the tab is opened
+   *
+   * @return The EIP graph component
+   */
+  def getPreferredFocusedComponent: JComponent = jpanel
+
+  /**
+   * Associates a name with the created editor tab
+   *
+   * @return A constant string for a better user experience
+   */
+  def getName: String = tabName
+
+  /**
+   * The EIP editor is stateless. As such, this is noop.
+   * @param level
+   * @return The default FileEditorState instance
+   */
+  def getState(level: FileEditorStateLevel): FileEditorState = FileEditorState.INSTANCE
+
+  /**
+   * The EIP diagram is readonly.
+   * @return always false
+   */
+  def isModified: Boolean = false
+
+  /**
+   * The EIP Diagram is readonly.
+   *
+   * @return true
+   */
+  def isValid: Boolean = true
+
+  /**
+   * Get the background highlightor for this editor
+   *
+   * @return Noop, no highlighter is required for the EIP diagram.
+   */
+  def getBackgroundHighlighter: BackgroundEditorHighlighter = null
+
+  /**
+   * Represents the user's location within the editor
+   * @return null, as the editor is not text based
+   */
+  def getCurrentLocation: FileEditorLocation = null
+
+  /**
+   * Get the structure view builder
+   *
+   * @return null, as the EIP does not have a structured view, as the EIP represents a tree already.
+   */
+  def getStructureViewBuilder: StructureViewBuilder = null
+}
---
+++ src/foo/editor/EipEditorProvider.scala
@@ -0,0 +1,79 @@
+package foo.editor
+
+import com.intellij.openapi.fileEditor.{FileEditor, FileEditorState, FileEditorPolicy, FileEditorProvider}
+import com.intellij.openapi.project.{Project, DumbAware}
+import org.jdom.Element
+import com.intellij.openapi.vfs.VirtualFile
+
+/**
+ * Represents the EipEditorProvider, which acts as a factory for FileEditors.
+ * Registered via the extension point fileEditorProvider
+ */
+class EipEditorProvider extends FileEditorProvider with DumbAware {
+  /**
+   * The unique Editor ID.
+   */
+  val editorId = "EipEditorProvider"
+
+  /**
+   * The EIP editor is stateless. As such, this is noop.
+   *
+   * @param state
+   * @param project
+   * @param targetElement
+   */
+  def writeState(state: FileEditorState, project: Project, targetElement: Element) {
+
+  }
+
+  /**
+   * Disposes the resources associated with the editor
+   * @param editor
+   */
+  def disposeEditor(editor: FileEditor) = editor.dispose()
+
+  /**
+   * Accepts all files which are known to be able to produce an EIP diagram.
+   * If this file is accepted, createdEditor will be called.
+   *
+   * @param project
+   * @param file
+   * @return
+   */
+  // TODO Convert to DOM
+  def accept(project: Project, file: VirtualFile): Boolean = true
+
+  /**
+   * Creates the EIP Graph editor associated with the virtual file.
+   * Note this method should only be called after accept results in true.
+   *
+   * @param project
+   * @param file
+   * @return The new file editor associated to the given file
+   */
+  def createEditor(project: Project, file: VirtualFile): FileEditor = new EipEditor(project, file)
+
+  /**
+   * Loads the state from the specified sourceElement.
+   * The EIP editor is stateless. As such, this is noop.
+   *
+   * @param sourceElement
+   * @param project
+   * @param file
+   * @return The default FileEditorState instance
+   */
+  def readState(sourceElement: Element, project: Project, file: VirtualFile): FileEditorState = FileEditorState.INSTANCE
+
+  /**
+   *
+   * @return The unique EipEditorProvider ID
+   */
+  def getEditorTypeId: String = editorId
+
+  /**
+   * Defines in which order our editor tab should appear.
+   * By default we wish to allow the user to choose the EIP tab themselves.
+   * @return #FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR
+   */
+  def getPolicy: FileEditorPolicy = FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR
+}
