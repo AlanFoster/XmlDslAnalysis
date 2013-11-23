3a52824313786df8e35423a0c98af51b97424145
Alan Foster <alan@alanfoster.me>
2013-11-23 19:37:08 +0000

    Update completion to align with Camel 2.12 documentation

--- src/foo/language/completion/CamelVariableCompletionContributor.scala
+++ src/foo/language/completion/CamelVariableCompletionContributor.scala
@@ -17,6 +17,8 @@ import foo.language.CamelTypes
 class CamelVariableCompletionContributor extends CompletionContributor {
   import PlatformPatterns._
 
+  case class Completion(lookupString: String, tail: String = "")
+
   val VARIABLE = psiElement().inside(classOf[CamelCamelVariable])
 
   val OPERATOR = psiElement().withParent(classOf[CamelCamelExpression])
@@ -37,25 +39,68 @@ class CamelVariableCompletionContributor extends CompletionContributor {
   addCompletion(
     VARIABLE,
     List(
-      "body",
-      "in",
-      "in.body",
-      "headers",
-      "in.headers"
+     Completion("camelId"),
+     Completion("camelContext.OGNL"),
+     Completion("exchangeId"),
+     Completion("id"),
+     Completion("body"),
+     Completion("in.body"),
+     Completion("body.OGNL"),
+     Completion("in.body.OGNL"),
+     Completion("bodyAs", "(type)"),
+     Completion("mandatoryBodyAs", "(type)"),
+     Completion("out.body"),
+     Completion("header.foo"),
+     Completion("header[foo]"),
+     Completion("headers.foo"),
+     Completion("headers[foo]"),
+     Completion("in.header.foo"),
+     Completion("in.header[foo]"),
+     Completion("in.headers.foo"),
+     Completion("in.headers[foo]"),
+     Completion("header.foo[bar]"),
+     Completion("in.header.foo[bar]"),
+     Completion("in.headers.foo[bar]"),
+     Completion("header.foo.OGNL"),
+     Completion("in.header.foo.OGNL"),
+     Completion("in.headers.foo.OGNL"),
+     Completion("out.header.foo"),
+     Completion("out.header[foo]"),
+     Completion("out.headers.foo"),
+     Completion("out.headers[foo]"),
+     Completion("headerAs", "(key,type)"),
+     Completion("headers"),
+     Completion("in.headers"),
+     Completion("property.foo"),
+     Completion("property[foo]"),
+     Completion("property.foo.OGNL"),
+     Completion("sys.foo"),
+     Completion("sysenv.foo"),
+     Completion("exception"),
+     Completion("exception.OGNL"),
+     Completion("exception.message"),
+     Completion("exception.stacktrace"),
+     Completion("date:command:pattern"),
+     Completion("bean:bean"),
+     Completion("properties:locations:key"),
+     Completion("routeId"),
+     Completion("threadName"),
+     Completion("ref:xxx"),
+     Completion("type:name.field")
     )
   )
 
   // TODO We potentially have this list already within the camel syntax highlighting
-  addCompletion(
+ addCompletion(
     OPERATOR,
     List(
-      CamelBaseElementType.getName(CamelTypes.AND_AND),
-      CamelBaseElementType.getName(CamelTypes.OR_OR),
-      CamelBaseElementType.getName(CamelTypes.EQ_EQ),
-      CamelBaseElementType.getName(CamelTypes.GT),
-      CamelBaseElementType.getName(CamelTypes.GT_EQ),
-      CamelBaseElementType.getName(CamelTypes.LT),
-      CamelBaseElementType.getName(CamelTypes.LT_EQ)
+      Completion(CamelBaseElementType.getName(CamelTypes.AND_AND)),
+      Completion(CamelBaseElementType.getName(CamelTypes.OR_OR)),
+      Completion(CamelBaseElementType.getName(CamelTypes.EQ_EQ)),
+      Completion(CamelBaseElementType.getName(CamelTypes.GT)),
+      Completion(CamelBaseElementType.getName(CamelTypes.GT_EQ)),
+      Completion(CamelBaseElementType.getName(CamelTypes.LT)),
+      Completion(CamelBaseElementType.getName(CamelTypes.LT_EQ))
     )
   )
 
@@ -71,14 +116,23 @@ class CamelVariableCompletionContributor extends CompletionContributor {
    * @param elementPattern The element pattern to use
    * @param completions The possible completions
    */
-  def addCompletion(elementPattern: ElementPattern[_ <: PsiElement], completions: List[String]) {
+  def addCompletion(elementPattern: ElementPattern[_ <: PsiElement], completions: List[Completion]) {
     extend(CompletionType.BASIC,
       elementPattern,
       new CompletionProvider[CompletionParameters]() {
         def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
-          completions.foreach(label => result.addElement(LookupElementBuilder.create(label)))
+          completions.foreach(completion => {
+            val lookupBuilder =
+              LookupElementBuilder
+                .create(completion.lookupString)
+                .appendTailText(completion.tail, false)
+
+             result.addElement(lookupBuilder)
+          })
         }
       }
     )
   }
 }
+
+
