915cf2d9c039245e5785973992327eba6375378d
Alan Foster <alan@alanfoster.me>
2014-01-04 03:19:32 +0000

    Fix broken build

--- src/test/scala/foo/FooTest.scala
+++ src/test/scala/foo/FooTest.scala
@@ -3,14 +3,25 @@ package foo
 import junit.framework.{Assert, TestCase}
 import com.intellij.testFramework.PlatformTestCase
 import com.intellij.idea.IdeaTestApplication
+import scala.collection.JavaConverters._
 
 class FooTest extends TestCase {
-  def ignoredJVMArgs() {
-    Assert.assertEquals("JVM Arg should be set!", "bar", System.getProperty("foo"))
+  def testJVMArgs() {
+    Assert.assertEquals("JVM Arg bar should be set!", "bar", System.getProperty("foo"))
+    Assert.assertEquals("JVM idea.home.path should be set!",
+      "C:\\Users\\alan\\.IntelliJIdea13\\system\\plugins-sandbox\\test",
+      System.getProperty("idea.home.path"))
+    Assert.assertEquals("JVM idea.plugins.path should be set!",
+      "C:\\Users\\alan\\.IntelliJIdea13\\system\\plugins-sandbox\\plugins",
+      System.getProperty("idea.plugins.path"))
+    Assert.assertNotNull("Java home should not be null",
+      System.getProperty("java.home")
+    )
+
   }
 
   // TODO investigate further cucumber https://github.com/JetBrains/intellij-community/blob/master/plugins/git4idea/test-stepdefs/git4idea/GitCucumberWorld.java
-  def testIntellij() {
+  def ignoreTestIntellij() {
     PlatformTestCase.initPlatformLangPrefix()
     IdeaTestApplication.getInstance(null)
    // println("" + PathManager.getBinPath())
