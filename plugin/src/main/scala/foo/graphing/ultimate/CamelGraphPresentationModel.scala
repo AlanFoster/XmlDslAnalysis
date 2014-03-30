7ee6b53b25ab9de6b2056de6e5e31a5f24cac4e2
Alan Foster <alan@alanfoster.me>
2014-03-30 15:46:02 +0100

    Further investigation into drawing IJ ultimate graphs

--- plugin/src/main/scala/foo/graphing/ultimate/CamelGraphPresentationModel.scala
+++ plugin/src/main/scala/foo/graphing/ultimate/CamelGraphPresentationModel.scala
@@ -3,15 +3,13 @@ package foo.graphing.ultimate
 import com.intellij.openapi.graph.builder.components.BasicGraphPresentationModel
 import com.intellij.openapi.graph.base.Graph
 import foo.eip.graph.EipProcessor
-import foo.eip.graph.ADT.Edge
-import com.intellij.openapi.graph.view.{EditMode, Graph2DView, NodeRealizer}
-import com.intellij.openapi.graph.builder.util.GraphViewUtil
+import com.intellij.openapi.graph.view._
 import com.intellij.openapi.graph.settings.GraphSettingsProvider
-import com.intellij.openapi.project.{Project, ProjectManager}
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.graph.GraphManager
-import com.intellij.openapi.graph.layout.hierarchic.HierarchicLayouter
-import com.intellij.openapi.graph.layout.{Layouter, OrientationLayouter}
-import com.intellij.openapi.graph.builder.EdgeCreationPolicy
+import java.awt.Color
+import foo.eip.graph.ADT.Edge
+import com.intellij.openapi.graph.builder.util.GraphViewUtil
 
 /**
  * The graph presentation model deals with edges/nodes drawing etc.
@@ -23,13 +21,24 @@ class CamelGraphPresentationModel(graph: Graph, project: Project)
   override def customizeSettings(view: Graph2DView, editMode: EditMode): Unit = {
     super.customizeSettings(view, editMode)
 
+    // Disable various view/editing modes etc
+    view.setGridVisible(false)
+    view.setAntialiasedPainting(false)
+    view.setFitContentOnResize(false)
+
+    editMode.allowResizeNodes(false)
+    editMode.allowEdgeCreation(false)
+    editMode.allowBendCreation(false)
+
+    // Bind our default layout - ie bubble/hierarchical etc
+    val layouter = getLayouter
     GraphSettingsProvider.getInstance(project).getSettings(graph)
-      .setCurrentLayouter(getLayouter)
+      .setCurrentLayouter(layouter)
   }
 
-  def getLayouter = {
-    val layouter = GraphManager.getGraphManager.createHierarchicLayouter()
-    layouter.setMinimalNodeDistance(50)
+  lazy val getLayouter = {
+    val layouter = GraphManager.getGraphManager.createHierarchicGroupLayouter()
+    layouter.setMinimalNodeDistance(80)
     layouter.setMinimalLayerDistance(50)
     layouter.setOrientationLayouterEnabled(true)
     layouter
@@ -49,8 +58,71 @@ class CamelGraphPresentationModel(graph: Graph, project: Project)
    * @return A custom node realizer for this node
    */
   override def getNodeRealizer(eipProcessor: EipProcessor): NodeRealizer = {
-    GraphViewUtil.createNodeRealizer("EipProcessorRenderer", eipGraphNodeRenderer)
+   // GraphManager.getGraphManager.createGenericNodeRealizer()
+    //val implementations = GenericNodeRealizer.Statics.getFactory.getAvailableConfigurations
+
+    // Get the default map assocaited with Realization components, and modify it with our pluggable instances
+    val factory = GenericNodeRealizer.Statics.getFactory
+    val defaultConfigurationMap = factory.createDefaultConfigurationMap().asInstanceOf[java.util.Map[Any, Any]]
+    defaultConfigurationMap.put(
+      classOf[GenericNodeRealizer.Painter],
+      GraphManager.getGraphManager.createNodeCellRendererPainter(eipGraphNodeRenderer, NodeCellRendererPainter.USER_DATA_MAP)
+    )
+
+/*
+    val impl = new AbstractCustomHotSpotPainter {
+      override def hotSpotHit(noderealizer: NodeRealizer, d: Double, d1: Double): Byte = ???
+      override def paintHotSpots(noderealizer: NodeRealizer, graphics2d: Graphics2D): Unit = ???
+    }
+*/
+
+/*    defaultConfigurationMap.put(
+      classOf[GenericNodeRealizer.HotSpotPainter],
+      //GraphManager.getGraphManager.createNodeCellRendererPainter(eipGraphNodeRenderer, NodeCellRendererPainter.USER_DATA_MAP)
+      GraphManager.getGraphManager.createColorRenderer()
+    )*/
+
+    factory.addConfiguration("EipPluggableConfiguration", defaultConfigurationMap)
+
+
+  // Create an instance of a generic realizer, and provide the configuration name
+   val realizor =  GraphManager.getGraphManager.createGenericNodeRealizer("EipPluggableConfiguration")
+
+    realizor
+
+    // GraphViewUtil.createNodeRealizer("EipProcessorRender", eipGraphNodeRenderer)
+    // http://docs.yworks.com/yfiles/doc/api/y/view/GenericNodeRealizer.html
+    // You seem to create a generic node realizer, then override what you want
+
+
+
+/*    val path = "file:\\C:\\Users\\a\\Documents\\GitHub\\XmlDslAnalysis\\plugin\\src\\main\\resources\\eip\\unpicked\\from.gif"*/
+    //val icon = (new Object with IntellijIconLoader).loadUnpickedIcon(eipProcessor.eipType.toString.toLowerCase)
+/*    val nodeRealizer = GraphManager.getGraphManager.createImageNodeRealizer()
+
+    //nodeRealizer.setImage(new ImageIcon(path).getImage)
+    nodeRealizer.setImageURL(new URL(path))
+    nodeRealizer.setAlphaImageUsed(true)
+    nodeRealizer.setToImageSize()*/
+/*
+    nodeRealizer*/
   }
 
+  override def editEdge(e: Edge[EipProcessor, String]): Boolean = {
+    false
+  }
+  override def editNode(n: EipProcessor): Boolean = false
 
+  override def getEdgeRealizer(e: Edge[EipProcessor, String]): EdgeRealizer = {
+    val edgeRealizer = GraphManager.getGraphManager.createPolyLineEdgeRealizer()
+    edgeRealizer.setSmoothedBends(true)
+    edgeRealizer.setLineType(LineType.LINE_1)
+    edgeRealizer.setLineColor(Color.DARK_GRAY)
+    edgeRealizer.setSourceArrow(Arrow.NONE)
+    edgeRealizer.setTargetArrow(Arrow.STANDARD)
+
+    edgeRealizer
+  }
+
+  override def getNodeTooltip(n: EipProcessor): String = n.text
 }
--- plugin/src/main/scala/foo/graphing/ultimate/EipGraphNodeRenderer.scala
+++ plugin/src/main/scala/foo/graphing/ultimate/EipGraphNodeRenderer.scala
@@ -1,32 +1,89 @@
 package foo.graphing.ultimate
 
-import com.intellij.openapi.graph.builder.renderer.BasicGraphNodeRenderer
+import com.intellij.openapi.graph.builder.renderer.{BasicNodeCellRenderer, BasicGraphNodeRenderer}
 import foo.eip.graph.EipProcessor
 import foo.eip.graph.ADT.Edge
 import com.intellij.openapi.util.ModificationTracker
 import com.intellij.openapi.graph.builder.GraphBuilder
-import javax.swing.{JComponent, Icon}
+import javax.swing._
 import foo.eip.graph.loaders.IntellijIconLoader
-import com.intellij.openapi.graph.view.NodeRealizer
+import com.intellij.openapi.graph.view.{NodeCellRenderer, Graph2DView, NodeRealizer}
+import java.awt.{GridBagConstraints, Component, BorderLayout, Color}
+import com.intellij.ui.{JBColor, LightColors}
+import foo.eip.graph.ADT.Edge
+import com.intellij.ui.components.panels.NonOpaquePanel
 
 /**
  * A concrete implementation of an EipProcessor renderer,
  * which has the ability to draw a given node in a graph
- * @param builder The graph builder currently being used to create a graph
+ * @param der The graph builder currently being used to create a graph
  */
 class EipGraphNodeRenderer(builder: GraphBuilder[EipProcessor, Edge[EipProcessor, String]])
-  extends BasicGraphNodeRenderer[EipProcessor, Edge[EipProcessor, String]](
-    builder,
-    ModificationTracker.NEVER_CHANGED
-  ) {
+  extends NodeCellRenderer {
+//  extends BasicGraphNodeRenderer[EipProcessor, Edge[EipProcessor, String]](
+    // builder,
+ //   ModificationTracker.NEVER_CHANGED
+ // ) {
 
-  override def getIcon(node: EipProcessor): Icon = {
+/*  override def getIcon(node: EipProcessor): Icon = {
     val icon = (new Object with IntellijIconLoader).loadUnpickedIcon(node.eipType.toString.toLowerCase)
     icon
-  }
-
+  }*/
+/*
   override def getLabelPanel(nodeRealizer: NodeRealizer): JComponent = {
     nodeRealizer.setLabelText("")
     super.getLabelPanel(nodeRealizer)
+  }*/
+/*  override def getIconLabel(node: EipProcessor): JComponent = {
+
+  }*/
+/*  override def tuneNode(nodeRealizer: NodeRealizer, wrapper: JPanel): Unit = {
+    wrapper.removeAll()
+    //wrapper.add(new JButton("Foo: " + builder.getNodeObject(nodeRealizer.getNode).text))
+  val button = new JButton()
+  button.setText("             ")
+  button.setBackground(Color.RED)
+    wrapper.add(button)
+  }
+
+  override def getBackground(node: EipProcessor): Color = {
+    LightColors.RED
+  }
+
+  override def getSelectionColor: Color = {
+    new JBColor(new Color(0xff00cc), new Color(0xff00cc))
+  }*/
+
+
+/*  override def getRendererComponent(graph2dview: Graph2DView, noderealizer: NodeRealizer, obj: scala.Any, isSelected: Boolean): JComponent = {
+    new JButton("I am currently selected : " + isSelected)
+  }*/
+
+  /**
+   * Serves as a cache for previously created NodeRealizers.
+   * The Boolean is associated with the fact that it is selected or not.
+   * The value is the already created JComponent
+   */
+  var cache = collection.mutable.Map[(NodeRealizer, Boolean), JComponent]()
+
+  def getNodeCellRendererComponent(graph: Graph2DView,
+                                   realizer: NodeRealizer,
+                                   obj: AnyRef,
+                                   isSelected: Boolean): JComponent = {
+    cache.getOrElseUpdate((realizer, isSelected), {
+      val node = builder.getNodeObject(realizer.getNode)
+      val label = {
+        val eipType = node.eipType.toString.toLowerCase
+        val icon =
+          if(isSelected) (new Object with IntellijIconLoader).loadPickedIcon(eipType)
+          else (new Object with IntellijIconLoader).loadUnpickedIcon(eipType)
+        val label = new JLabel(icon)
+        label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
+        label.setSize(icon.getIconHeight, icon.getIconHeight)
+        realizer.setSize(icon.getIconWidth, icon.getIconHeight)
+        label
+      }
+      label
+    })
   }
 }
